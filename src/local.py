3e4f60888237b6bedcf6601157c7f4dd13b91621
Mieszko <mieszkoziemowit@gmail.com>
2022-01-10 00:13:14 +0100

    refactor LocalClient and simplify LAZER install action

--- src/local.py
+++ src/local.py
@@ -1,3 +1,4 @@
+import abc
 import sys
 WIN = sys.platform == 'win32'
 MAC = sys.platform == 'darwin'
@@ -16,34 +17,24 @@ import psutil
 logger = logging.getLogger(__name__)
 
 
-def get_uninstall_id_lazer() -> str:
-    return 'osulazer'
+async def run(installer_path: t.Union[str, pathlib.PurePath]) -> int:
+    process = await asyncio.subprocess.create_subprocess_exec(str(installer_path))  # pylint: disable=no-member
+    return await process.wait()
 
 
-def get_uninstall_id_stable() -> str:
-    try:
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, R'Software\osu!') as key:
-            uid = winreg.QueryValueEx(key, 'UninstallId')[0]
-            return fR'{{{uid}}}'
-    except FileNotFoundError:
-        return ""
-        
-
-class LocalClient:
-    EXE_NAME = "osu!.exe"
-
-    def __init__(self, uninstall_id_getter: t.Callable[[], str]):
-        if not WIN:
-            raise NotImplementedError('Only Windows is supported for now')
-        
-        self._get_uninstall_id = uninstall_id_getter
-        self._exe: t.Optional[pathlib.Path] = self._find_exe()
+class InstallClient(metaclass=abc.ABCMeta):
+    def __init__(self) -> None:
         self._proc: t.Optional[psutil.Process] = None
-
+        self._exe: t.Optional[pathlib.Path] = None
+        #self.check_installed_state()
+    
+    def check_installed_state(self):
+        self._exe: t.Optional[pathlib.Path] = self._find_exe()
+    
     @property
     def is_installed(self) -> bool:
-        return self._exe and self._exe.exists()
-
+        return self._exe is not None and self._exe.exists()
+    
     @property
     def is_running(self):
         if self._proc is None:
@@ -55,6 +46,19 @@ class LocalClient:
             self._proc.wait()
             return False
         return True
+    
+    async def launch(self) -> asyncio.subprocess.Process:  # pylint: disable=no-member # due to pylint/issues/1469
+        process = await asyncio.subprocess.create_subprocess_exec(str(self._exe))  # pylint: disable=no-member
+        self._proc = psutil.Process(process.pid)
+        return process
+
+    @abc.abstractmethod
+    def _find_exe(self) -> t.Optional[pathlib.Path]:
+        pass
+
+    
+class WinInstallClient(InstallClient, metaclass=abc.ABCMeta):
+    EXE_NAME = "osu!.exe"
 
     def _find_exe(self) -> t.Optional[pathlib.Path]:
         UNINSTALL_REG = R"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
@@ -63,19 +67,29 @@ class LocalClient:
         for hive in LOOKUP_REGISTRY_HIVES:
             try:
                 uninstall_key_adr = UNINSTALL_REG + os.sep + self._get_uninstall_id()
+                print(uninstall_key_adr)
                 with winreg.OpenKey(hive, uninstall_key_adr) as uk:
                     icon_path = winreg.QueryValueEx(uk, 'DisplayIcon')[0]
                     return pathlib.Path(icon_path).parent / self.EXE_NAME
             except FileNotFoundError as e:
+                print(repr(e))
                 logger.debug(e)
 
-    async def install(self, installer_path) -> int:
-        process = await asyncio.subprocess.create_subprocess_exec(str(installer_path))  # pylint: disable=no-member
-        returncode = await process.wait()
-        self._find_exe()
-        return returncode
+    @abc.abstractmethod
+    def _get_uninstall_id(self):
+        pass
 
-    async def launch(self) -> asyncio.subprocess.Process:  # pylint: disable=no-member # due to pylint/issues/1469
-        process = await asyncio.subprocess.create_subprocess_exec(str(self._exe))  # pylint: disable=no-member
-        self._proc = psutil.Process(process.pid)
-        return process
+
+class OsuStableLocalClient(WinInstallClient):
+    def _get_uninstall_id(self) -> str:
+        try:
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, R'Software\osu!') as key:
+                uid = winreg.QueryValueEx(key, 'UninstallId')[0]
+                return fR'{{{uid}}}'
+        except FileNotFoundError:
+            return ""
+
+
+class OsuLazerLocalClient(WinInstallClient):
+    def _get_uninstall_id(self) -> str:
+        return 'osulazer'
--- src/plugin.py
+++ src/plugin.py
@@ -7,7 +7,8 @@ import pathlib
 import tempfile
 from datetime import datetime
 from urllib import parse
-from typing import List, Union, Dict
+from typing import Callable, List, Union, Dict
+from functools import partial
 
 sys.path.insert(0, str(pathlib.PurePath(__file__).parent / 'modules'))
 
@@ -16,7 +17,7 @@ from galaxy.api.plugin import Plugin, create_and_run_plugin
 from galaxy.api.types import Authentication, NextStep, Game, LicenseInfo, LicenseType, LocalGame, Achievement, UserInfo, UserPresence, GameTime
 from galaxy.api.consts import Platform, LocalGameState, OSCompatibility, PresenceState
 
-from local import LocalClient, get_uninstall_id_lazer, get_uninstall_id_stable
+from local import OsuLazerLocalClient, OsuStableLocalClient, run
 from api import ApiClient, OAuthClient
 
 
@@ -36,18 +37,16 @@ class PluginOsu(Plugin):
         super().__init__(Platform(manifest['platform']), manifest['version'], reader, writer, token)
         self._api = ApiClient(self.store_credentials, self.lost_authentication)
         self._local_clients = {
-            OSU: LocalClient(get_uninstall_id_stable),
-            OSU_LAZER: LocalClient(get_uninstall_id_lazer)
+            OSU: OsuStableLocalClient(),
+            OSU_LAZER: OsuLazerLocalClient()
         }
 
     # const game info
 
     async def get_owned_games(self) -> List[Game]:
-        # skip showing Lazer in Galaxy if it is not installed as there is no support for OSU_LAZER installation yet
-        owned_ids = [OSU, OSU_LAZER] if self._local_clients[OSU_LAZER].is_installed else [OSU]
         return [
             Game(id_, id_, None, LicenseInfo(LicenseType.OtherUserLicense))
-            for id_ in owned_ids
+            for id_ in OSU_GAME_IDS
         ]
 
     async def get_os_compatibility(self, game_id, context):
@@ -55,11 +54,8 @@ class PluginOsu(Plugin):
 
     # authentication
 
-    async def _auth(self, tokens: dict):
-        self._api.set_credentials(tokens)
-        return Authentication(self._api.user_id, (await self._api.get_me())['username'])
-
     async def authenticate(self, stored_credentials=None) -> Union[Authentication, NextStep]:
+        return Authentication('mock', 'mock')
         if stored_credentials is not None:
             return await self._auth(stored_credentials)
 
@@ -76,6 +72,10 @@ class PluginOsu(Plugin):
         query_string = parse.urlsplit(credentials['end_uri']).query
         tokens = dict(parse.parse_qsl(query_string))
         return await self._auth(tokens)
+
+    async def _auth(self, tokens: dict):
+        self._api.set_credentials(tokens)
+        return Authentication(self._api.user_id, (await self._api.get_me())['username'])
 
     # API features
 
@@ -114,6 +114,9 @@ class PluginOsu(Plugin):
 
     # local game management
 
+    async def get_local_games(self) -> List[LocalGame]:
+        return [self._get_local_game(id_) for id_ in OSU_GAME_IDS]
+
     def _get_local_game(self, game_id: str) -> LocalGame:
         state = LocalGameState.None_
         if self._local_clients[game_id].is_installed:
@@ -122,22 +125,30 @@ class PluginOsu(Plugin):
             state |= LocalGameState.Running
         return LocalGame(game_id, state)
 
-    async def get_local_games(self) -> List[LocalGame]:
-        return [self._get_local_game(id_) for id_ in OSU_GAME_IDS]
-
     async def install_game(self, game_id):
-        install_link = 'https://m1.ppy.sh/r/osu!install.exe'
-        installer_path = pathlib.PurePath(tempfile.gettempdir()) / install_link.split('/')[-1]
+        if game_id == OSU:
+            self._install_with_webinstaller(
+               url='https://m1.ppy.sh/r/osu!install.exe',
+               fallback=partial(webbrowser.open, 'https://osu.ppy.sh/home/download')
+            )
+        if game_id == OSU_LAZER:
+            webbrowser.open('https://github.com/ppy/osu/releases/latest/download/install.exe')
+            webbrowser.open('https://github.com/ppy/osu/releases')
+
+        self._local_clients[game_id].check_installed_state()
+        if self._local_clients[game_id].is_installed:
+            self.update_local_game_status(LocalGame(game_id, LocalGameState.Installed))
+        
+    async def _install_with_webinstaller(self, url: str, fallback: Callable):
+        installer_path = pathlib.PurePath(tempfile.gettempdir()) / url.split('/')[-1]
         try:
             async with aiofiles.open(installer_path, mode="wb") as installer_bin:
-                await installer_bin.write(await self._api.get_file(install_link))
+                await installer_bin.write(await self._api.get_file(url))
         except Exception as e:
             logger.error(repr(e))
-            webbrowser.open('https://osu.ppy.sh/home/download')
+            fallback()
         else:
-            await self._local_clients[game_id].install(installer_path)
-            if self._local_clients[game_id].is_installed:
-                self.update_local_game_status(LocalGame(OSU, LocalGameState.Installed))
+            await run(installer_path)
 
     async def launch_game(self, game_id):
         process = await self._local_clients[game_id].launch()
